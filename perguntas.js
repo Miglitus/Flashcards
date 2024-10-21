939082ecf98ec0d04ebbd3c9d8eb2d672e258a99
Miglitus <138683470+Miglitus@users.noreply.github.com>
2024-10-21 11:41:00 -0300

    11

--- perguntas.js
+++ perguntas.js
@@ -1,3 +1,5 @@
 criarCartao{
-    
+    'Ciências'
+    'O que ciências?'
+    ' A ciência é uma forma sistemática de aquisição de conhecimentos, baseada em um método objetivo e bem definido, conhecido como método científico.'
 }
