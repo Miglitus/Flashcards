27ed2837c24a1d3eeaad353a90097232e6f724bf
Miglitus <138683470+Miglitus@users.noreply.github.com>
2024-10-28 11:51:14 -0300

    '1111

--- perguntas.js
+++ perguntas.js
@@ -1,5 +1,53 @@
-criarCartao{
-    'Ciências'
-    'O que ciências?'
+criarCartao(
+    'Ciências',
+    'O que ciências?',
     'A ciência é uma forma sistemática de aquisição de conhecimentos, baseada em um método objetivo e bem definido, conhecido como método científico.'
-}
+)
+
+criarCartao(
+    'História',
+    'Quem foi Adolf Hitler?',
+    'Adolf Hitler, foi um político alemão que serviu como líder do Partido Nazista, Chanceler do Reich e Führer da Alemanha Nazista de 1934 até 1945. Como ditador do Reich Alemão, ele foi o principal instigador da Segunda Guerra Mundial na Europa e figura central do Holocausto.'
+)
+
+criarCartao(
+    'Matemática',
+    'Quanto é 1+1?',
+    'É 2'
+)    
+
+criarCartao(
+    'Ciências',
+    'O que é a vida?',
+    'Em termos da definição Fisiológica, a vida foi definida como qualquer sistema capaz de executar um número de funções tais como: Alimentação, metabolização, excreção, respiração, movimento, crescimento, reprodução, resposta a estímulos externos.'
+)
+
+criarCartao(
+    'História',
+    'Quando se iniciou  primeira Guerra Mundial?',
+    '28 de julho de 1914'
+)
+
+criarCartao(
+    'Matemática',
+    'Quanto é 2x4?',
+    'É 8'
+)
+
+criarCartao(
+    'História',
+    'Quando se iniciou a segunda Guerra Mundial?',
+    '1 de setembro de 1939.'
+)
+
+criarCartao(
+    'Ciencias',
+    'O que é o espematozóide?',
+    'O espermatozoide é uma célula microscópica, constituída por três partes principais: cabeça, peça intermediária e cauda.'
+)
+
+criarCartao(
+    'Matemática',
+    'Quanto é 25x10?',
+    '250'
+)
