0ffcdae7aa704b73668bead457f1b0b0d05eb19a
Miglitus <138683470+Miglitus@users.noreply.github.com>
2024-11-01 13:40:29 -0300

    1

---
+++ app.js
@@ -0,0 +1,16 @@
+function criarCartao(categoria, pergunta, resposta){
+    let container = document.getElementById('container')
+    let cartao = document.getElementById('article')
+    cartao.className = 'cartao'
+
+    cartao
+    <div class="cartao__conteudo">
+                    <h3>Programação</h3>
+                    <div class="cartao_conteudo_pergunta">
+                        <p>O que é JavaScript?</p>
+                    </div>
+                    <div class="cartao_conteudo_resposta">
+                        <p>O JavaScript é uma linguagem de programação</p>
+                    </div>
+                </div>
+}
